5b6d9a61745eb6a11fadb939ba9d1caee8f6dab7
Andreas Dørum <andreas.doerum@gmail.com>
2016-03-01 11:32:32 +0100

    add own normalize function

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,6 +1,5 @@
-from numpy import matrix, transpose, zeros, ones
+from numpy import matrix, transpose, zeros, ones, newaxis
 from numpy.ma import multiply
-from sklearn.preprocessing import normalize
 
 transition_model = matrix([[0.7, 0.3],
                            [0.3, 0.7]])
@@ -18,7 +17,7 @@ def forward(belief_state, new_evidence):
     # not_normal = multiply(sensor_model[new_evidence], transpose(prediction))
 
     f = sensor_model[new_evidence] * transition_model * belief_state
-    normal = normalize(transpose(f), norm='l1')
+    normal = normalize(transpose(f))
     # print("evidence= %s \n f=%s \n normal=%s " % (sensor_model[new_evidence], f, normal))
 
     return transpose(normal)
@@ -31,6 +30,13 @@ def backward(b, evidence):
     return new_b
 
 
+def normalize(old_matrix):
+    row_sum = old_matrix.sum()
+    n = 1/row_sum
+    new_matrix = old_matrix * n
+    return new_matrix
+
+
 def forward_backward(evidence_sequence, prior):
     t = len(evidence_sequence)
     forward_messages = []  # forward messages
@@ -39,15 +45,16 @@ def forward_backward(evidence_sequence, prior):
     smoothed_estimates = [0] * (t + 1)  #
 
     forward_messages.append(prior)
-    for i in range(1, t+1):
-        print("i=%s" % i)
-        forward_messages.append(forward(forward_messages[i - 1], evidence_sequence[i-1]))
+    for i in range(1, t + 1):
+        forward_messages.append(forward(forward_messages[i - 1], evidence_sequence[i - 1]))
 
+    print("forwarded= %s" % forward_messages)
     for j in range(t, 1, -1):
-        normal = multiply(forward_messages[j], transpose(b))
-        print("j=%s \n forward=%s \n b=%s \n normal=%s" % (j, forward_messages[j], b, normal))
+        normal = multiply(forward_messages[j], b)
+        normal = normalize(transpose(normal))
+        print("j=%s forward=%s b=%s normal=%s" % (j, forward_messages[j], b, normal))
         smoothed_estimates[j] = normal
-        b = backward(b, evidence_sequence[j-1])
+        b = backward(b, evidence_sequence[j - 1])
 
     return smoothed_estimates
 
@@ -58,7 +65,7 @@ def forward_backward(evidence_sequence, prior):
 # print("rain on day1, given umbrella: %s \n rain on day2 given umbrella: %s" % (r1, r2))
 
 evi = [0, 0]
-pri = [[0.5], [0.5]]
+pri = matrix([[0.5], [0.5]])
 
 res = forward_backward(evi, pri)
 print("result=%s" % res)
